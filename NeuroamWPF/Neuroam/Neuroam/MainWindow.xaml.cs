dcaac07abeee9b97052306f8d4eb2737b79493b5
Abhishek Porwal <abhishek.porwal@ubisoft.com>
2019-11-04 17:43:41 -0500

    First init

---
+++ NeuroamWPF/Neuroam/Neuroam/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neuroam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> m_Items = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainTextBox.TextChanged += MainTextBox_TextChanged;
+        }
+
+        private void MainTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ResultsListBox.ItemsSource = MainTextBox.Text;
+        }
+    }
+}
---
+++ scratch.md
@@ -0,0 +1,8 @@
+#Neuroam
+A tool that allows you to store and retrieve your search/findings.
+
+#Language
+C#
+
+#External APIs
+JsonCpp
